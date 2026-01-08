324d04b9fb44e2b1a6c303e1d077cf3edb04fc8d
practcode <66366710+shekhar613@users.noreply.github.com>
2026-01-08 18:00:16 +0530

    Home page UI upadated

--- lib/body.dart
+++ lib/body.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:my_app/pages/allTopics_screen.dart';
-import 'package:my_app/pages/topics.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Body extends StatefulWidget {
